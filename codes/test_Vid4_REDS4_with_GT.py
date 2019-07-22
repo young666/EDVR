ba2ef2fe3ffa931de87217697f45baeaee328d87
Tai <lythanhtaiyoung666@gmail.com>
2019-07-22 04:53:01 +0000

    fix dataset path

--- codes/test_Vid4_REDS4_with_GT.py
+++ codes/test_Vid4_REDS4_with_GT.py
@@ -88,8 +88,8 @@ def main():
         test_dataset_folder = "../datasets/Vid4/BIx4"
         GT_dataset_folder = "../datasets/Vid4/GT"
     elif data_mode == "licensePlate_blur_bicubic":
-        test_dataset_folder = "../datasets/licensePlate2/BIx4"
-        GT_dataset_folder = "../datasets/licensePlate2/GT"
+        test_dataset_folder = "../datasets/license_plate2/BIx4"
+        GT_dataset_folder = "../datasets/license_plate2/GT"
     else:
         if stage == 1:
             test_dataset_folder = "../datasets/REDS4/{}".format(data_mode)
