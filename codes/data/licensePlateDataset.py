5bbb6bcef22ffaa153233ba7e917504789b03eeb
Tai <lythanhtaiyoung666@gmail.com>
2019-08-28 16:44:02 +0700

    fix data dimension

--- codes/data/licensePlateDataset.py
+++ codes/data/licensePlateDataset.py
@@ -197,14 +197,14 @@ class LicensePlateDataset(data.Dataset):
             img_GT = self._read_img_mc_BGR(self.GT_root, name_a, name_b)
             img_GT = img_GT.astype(np.float32) / 255.0
         elif self.data_type == "lmdb":
-            img_GT = util.read_img(self.GT_env, key, (3, 280, 300))
+            img_GT = util.read_img(self.GT_env, key, (3, 280, 280))
         else:
             img_GT = util.read_img(
                 None, osp.join(self.GT_root, name_a, name_b + ".png")
             )
 
         #### get LQ images
-        LQ_size_tuple = (3, 70, 75) if self.LR_input else (3, 280, 300)
+        LQ_size_tuple = (3, 70, 70) if self.LR_input else (3, 280, 280)
         img_LQ_l = []
         for v in neighbor_list:
             img_LQ_path = osp.join(self.LQ_root, name_a, "{:06d}.png".format(v))
